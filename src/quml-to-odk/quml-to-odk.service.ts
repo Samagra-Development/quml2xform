cd3caad3799af0c85e39301f07f8d50e75d9755c
Abhishek Mishra <abhishek@luezoid.com>
2022-07-26 16:27:49 +0530

    Service: now supporting parsers for various type of questions; currently restricted to MCQ types

--- src/quml-to-odk/quml-to-odk.service.ts
+++ src/quml-to-odk/quml-to-odk.service.ts
@@ -1,11 +1,12 @@
-import { Injectable, Logger } from '@nestjs/common';
+import { BadRequestException, Injectable, Logger } from '@nestjs/common';
 import { GenerateFormDto } from './dto/generate-form.dto';
 import { HttpService } from '@nestjs/axios';
 import { lastValueFrom, map } from 'rxjs';
 import { exec } from 'child_process';
-import * as XLSX from 'xlsx';
 import { ConfigService } from '@nestjs/config';
 import { v4 as uuid } from 'uuid';
+import { McqParserService } from './services/mcq-parser.service';
+import { QuestionTypesEnum } from './enums/question-types.enum';
 
 @Injectable()
 export class QumlToOdkService {
@@ -13,8 +14,6 @@ export class QumlToOdkService {
   private readonly questionDetailsUrl: string;
   private readonly xlsxFilesPath: string;
   private readonly odkFormsPath: string;
-
-  private templateFileName: string; // name of the template form files being generated
 
   private readonly logger = new Logger(QumlToOdkService.name); // logger instance
 
@@ -39,17 +38,25 @@ export class QumlToOdkService {
   public async generate(filters: GenerateFormDto) {
     const questions = await this.fetchQuestions(filters);
     if (questions.result && questions.result.count) {
-      this.templateFileName = uuid(); // initialize the template name
-      const parsedResult = this.parseQuestionsForXslxForm(questions, filters);
-      const xlsxFormFile = this.createXslx(
-        parsedResult[0],
-        parsedResult[1],
-        parsedResult[2],
-        parsedResult[3],
+      const templateFileName = uuid(); // initialize the template name
+      let service;
+
+      // based on question type, we'll use different parsers
+      switch (filters.qType) {
+        case QuestionTypesEnum.MCQ:
+          service = new McqParserService(); // create the instance
+          break;
+        default:
+          throw BadRequestException; // ideally this part should be handled at validation level itself
+      }
+
+      const xlsxFormFile = service.createForm(
+        questions,
+        filters,
+        this.xlsxFilesPath + '/' + templateFileName + '.xlsx',
       );
 
-      const odkFormName = this.templateFileName + '.xml';
-      const odkFormFile = this.odkFormsPath + '/' + odkFormName;
+      const odkFormFile = this.odkFormsPath + '/' + templateFileName + '.xml';
       await this.convertExcelToOdkForm(xlsxFormFile, odkFormFile);
       return {
         xlsxFile: xlsxFormFile,
@@ -69,7 +76,7 @@ export class QumlToOdkService {
           se_boards: filters.boards,
           gradeLevel: filters.grades,
           subject: filters.subjects,
-          qType: 'MCQ', // we'll always fetch MCQ questions
+          qType: filters.qType,
           topic: filters.competencies,
         },
       },
@@ -170,252 +177,7 @@ export class QumlToOdkService {
       });
   }
 
-  /**
-   * Prepares the XSLX form sheets data as per the provided template file: https://docs.google.com/spreadsheets/d/1GIcHghGYUR1FZ31BD-LCXBNMzr1xdCmQ/edit#gid=1148196142
-   *
-   * @param questions
-   * @param filters
-   *
-   * @return Array arrays of arrays for all the 4 sheets as mentioned in sample
-   */
-  private parseQuestionsForXslxForm(
-    questions,
-    filters: GenerateFormDto,
-  ): Array<any> {
-    const surveySheetHeader = [
-      'type',
-      'name',
-      'label',
-      'required',
-      'constraint',
-      'constraint_message',
-      'hint',
-      'relevant',
-      'choice_filter',
-      'appearance',
-      'calculation',
-      'image',
-    ];
-    const choicesSheetHeader = ['list name', 'name', 'label'];
-    const settingsSheetHeader = [
-      'form_title',
-      'form_id',
-      'allow_choice_duplicates',
-    ];
-    let surveySheetArray = [surveySheetHeader];
-    const choicesSheetArray = [choicesSheetHeader];
-    const settingsSheetArray = [settingsSheetHeader];
-    let name =
-      filters.boards[0] + '_' + filters.grades[0] + '_' + filters.subjects[0];
-    name = name.replace(' ', '_').toLowerCase();
-    surveySheetArray.push([
-      'begin group',
-      name,
-      'नीचे दिए गए अनुच्छेद/प्रश्न को विद्यार्थी द्वारा पढ़कर उस पर आधारित प्रश्नों का उत्तर दिया जाना है। \n' +
-        '\n' +
-        'इस प्रक्रिया में मेंटर्स विद्यार्थियों को केवल प्रश्न समझने में सहयोग करेंगे। विद्यार्थी द्वारा बताये गए उत्तर को ऐप में मेंटर को ही अंकित करना होगा।',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      'field-list',
-      '',
-      '',
-    ]);
-    let index = 1;
-    const totalMarksCellArray = [];
-    const optionsMap = ['a', 'b', 'c', 'd'];
-    const optionRowsArray = [];
-    questions.result.questions.forEach((question) => {
-      const itemName = (
-        'ques_' +
-        filters.subjects[0] +
-        '_' +
-        index
-      ).toLowerCase();
-      const itemType = 'select_one ' + itemName;
-      const itemLabel = QumlToOdkService.cleanHtml(
-        question.editorState.question,
-      );
-      const itemRequired = 'yes';
-      const itemConstraint = '';
-      const itemConstraintMessage = '';
-      const itemHint = '';
-      const itemRelevant = '';
-      const itemChoiceFilter = '';
-      const itemAppearance = '';
-      const itemCalculation = '';
-      const itemImage = ''; // todo set for image
-      surveySheetArray.push([
-        itemType,
-        itemName,
-        itemLabel,
-        itemRequired,
-        itemConstraint,
-        itemConstraintMessage,
-        itemHint,
-        itemRelevant,
-        itemChoiceFilter,
-        itemAppearance,
-        itemCalculation,
-        itemImage,
-      ]);
-
-      // prepare `calculation` column string & push to $optionRows; we'll merge this to $surveySheetArray later
-      let correctOption = '';
-      for (let i = 0; i < question.editorState.options.length; i++) {
-        const option = question.editorState.options[i];
-        if (option.answer) {
-          correctOption = optionsMap[i];
-        }
-        choicesSheetArray.push([
-          itemName,
-          optionsMap[i],
-          QumlToOdkService.cleanHtml(option.value.body),
-        ]); // populate choices sheet
-      }
-      const optionItemName = itemName + '_ans';
-      totalMarksCellArray.push('${' + optionItemName + '}');
-      optionRowsArray.push([
-        'calculate',
-        optionItemName,
-        '',
-        '',
-        '',
-        '',
-        '',
-        '',
-        '',
-        '',
-        'if(${' + itemName + '} = ‘' + correctOption + "‘, '1', '0')",
-        '',
-      ]);
-
-      index++; // at last increment the index
-    }); // forEach() ends
-
-    surveySheetArray.push([
-      'end group',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-    ]);
-
-    surveySheetArray = surveySheetArray.concat(optionRowsArray);
-
-    // push hidden `total_marks` row to $optionRows
-    surveySheetArray.push([
-      'calculate',
-      'total_marks',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      totalMarksCellArray.join(' + '),
-      '',
-    ]);
-
-    // push the remaining rows
-    surveySheetArray = surveySheetArray.concat([
-      [
-        'hidden',
-        'ques_number',
-        questions.result.questions.length,
-        '',
-        '',
-        '',
-        '',
-        '',
-        '',
-        '',
-        '',
-        '',
-      ],
-      [
-        'hidden',
-        'total_ques',
-        'कुल प्रश्न = ' + questions.result.questions.length,
-        '',
-        '',
-        '',
-        '',
-        '',
-        '',
-        '',
-        '',
-        '',
-      ],
-      [
-        'hidden',
-        'total_correct',
-        'कुल प्रश्न जिसका सही उत्तर दिया ${total_marks}',
-        '',
-        '',
-        '',
-        '',
-        '',
-        '',
-        '',
-        '',
-        '',
-      ],
-      ['start', 'start', '', '', '', '', '', '', '', '', '', ''],
-      ['end', 'end', '', '', '', '', '', '', '', '', '', ''],
-      ['today', 'today', '', '', '', '', '', '', '', '', '', ''],
-    ]);
-
-    settingsSheetArray.push([name, 'g2m_w1', 'no']);
-
-    return [surveySheetArray, choicesSheetArray, [], settingsSheetArray];
-  }
-
-  /**
-   * Returns the absolute path of the .xlsx file created
-   * @param surveySheetArray
-   * @param choicesSheetArray
-   * @param mediaSheetArray
-   * @param settingsSheetArray
-   *
-   * @return string file (absolute path of the generated XSLX form)
-   */
-  private createXslx(
-    surveySheetArray: Array<any>,
-    choicesSheetArray: Array<any>,
-    mediaSheetArray: Array<any>,
-    settingsSheetArray: Array<any>,
-  ): string {
-    const filename = this.templateFileName + '.xlsx';
-    const file = this.xlsxFilesPath + '/' + filename;
-
-    const workbook = XLSX.utils.book_new();
-    const surveySheet = XLSX.utils.aoa_to_sheet(surveySheetArray);
-    const choicesSheet = XLSX.utils.aoa_to_sheet(choicesSheetArray);
-    const mediaSheet = XLSX.utils.aoa_to_sheet(mediaSheetArray);
-    const settingsSheet = XLSX.utils.aoa_to_sheet(settingsSheetArray);
-    XLSX.utils.book_append_sheet(workbook, surveySheet, 'survey');
-    XLSX.utils.book_append_sheet(workbook, choicesSheet, 'choices');
-    XLSX.utils.book_append_sheet(workbook, mediaSheet, 'media');
-    XLSX.utils.book_append_sheet(workbook, settingsSheet, 'settings');
-    XLSX.writeFileXLSX(workbook, file, { bookType: 'xlsx', type: 'file' });
-
-    return file;
-  }
-
-  private static cleanHtml(str: string, nbspAsLineBreak = false) {
+  public static cleanHtml(str: string, nbspAsLineBreak = false) {
     // Remove HTML characters since we are not converting HTML to PDF.
     return str
       .replace(/<\/?(?!\bstrong\b)\b\w+\b>/g, '')
---
+++ src/quml-to-odk/services/mcq-parser.service.ts
@@ -0,0 +1,249 @@
+import { Injectable} from '@nestjs/common';
+import { GenerateFormDto } from '../dto/generate-form.dto';
+import { QumlToOdkService } from '../quml-to-odk.service';
+import * as XLSX from 'xlsx';
+
+@Injectable()
+export class McqParserService {
+  /**
+   * Prepares the XSLX form sheets data as per the provided template file: https://docs.google.com/spreadsheets/d/1GIcHghGYUR1FZ31BD-LCXBNMzr1xdCmQ/edit#gid=1148196142
+   *
+   * @param questions
+   * @param filters
+   *
+   * @return Array arrays of arrays for all the 4 sheets as mentioned in sample
+   */
+  private parseQuestions(questions, filters: GenerateFormDto): Array<any> {
+    const surveySheetHeader = [
+      'type',
+      'name',
+      'label',
+      'required',
+      'constraint',
+      'constraint_message',
+      'hint',
+      'relevant',
+      'choice_filter',
+      'appearance',
+      'calculation',
+      'image',
+    ];
+    const choicesSheetHeader = ['list name', 'name', 'label'];
+    const settingsSheetHeader = [
+      'form_title',
+      'form_id',
+      'allow_choice_duplicates',
+    ];
+    let surveySheetArray = [surveySheetHeader];
+    const choicesSheetArray = [choicesSheetHeader];
+    const settingsSheetArray = [settingsSheetHeader];
+    let name =
+      filters.boards[0] + '_' + filters.grades[0] + '_' + filters.subjects[0];
+    name = name.replace(' ', '_').toLowerCase();
+    surveySheetArray.push([
+      'begin group',
+      name,
+      'नीचे दिए गए अनुच्छेद/प्रश्न को विद्यार्थी द्वारा पढ़कर उस पर आधारित प्रश्नों का उत्तर दिया जाना है। \n' +
+        '\n' +
+        'इस प्रक्रिया में मेंटर्स विद्यार्थियों को केवल प्रश्न समझने में सहयोग करेंगे। विद्यार्थी द्वारा बताये गए उत्तर को ऐप में मेंटर को ही अंकित करना होगा।',
+      '',
+      '',
+      '',
+      '',
+      '',
+      '',
+      'field-list',
+      '',
+      '',
+    ]);
+    let index = 1;
+    const totalMarksCellArray = [];
+    const optionsMap = ['a', 'b', 'c', 'd'];
+    const optionRowsArray = [];
+    questions.result.questions.forEach((question) => {
+      const itemName = (
+        'ques_' +
+        filters.subjects[0] +
+        '_' +
+        index
+      ).toLowerCase();
+      const itemType = 'select_one ' + itemName;
+      const itemLabel = QumlToOdkService.cleanHtml(
+        question.editorState.question,
+      );
+      const itemRequired = 'yes';
+      const itemConstraint = '';
+      const itemConstraintMessage = '';
+      const itemHint = '';
+      const itemRelevant = '';
+      const itemChoiceFilter = '';
+      const itemAppearance = '';
+      const itemCalculation = '';
+      const itemImage = ''; // todo set for image
+      surveySheetArray.push([
+        itemType,
+        itemName,
+        itemLabel,
+        itemRequired,
+        itemConstraint,
+        itemConstraintMessage,
+        itemHint,
+        itemRelevant,
+        itemChoiceFilter,
+        itemAppearance,
+        itemCalculation,
+        itemImage,
+      ]);
+
+      // prepare `calculation` column string & push to $optionRows; we'll merge this to $surveySheetArray later
+      let correctOption = '';
+      for (let i = 0; i < question.editorState.options.length; i++) {
+        const option = question.editorState.options[i];
+        if (option.answer) {
+          correctOption = optionsMap[i];
+        }
+        choicesSheetArray.push([
+          itemName,
+          optionsMap[i],
+          QumlToOdkService.cleanHtml(option.value.body),
+        ]); // populate choices sheet
+      }
+      const optionItemName = itemName + '_ans';
+      totalMarksCellArray.push('${' + optionItemName + '}');
+      optionRowsArray.push([
+        'calculate',
+        optionItemName,
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        'if(${' + itemName + '} = ‘' + correctOption + "‘, '1', '0')",
+        '',
+      ]);
+
+      index++; // at last increment the index
+    }); // forEach() ends
+
+    surveySheetArray.push([
+      'end group',
+      '',
+      '',
+      '',
+      '',
+      '',
+      '',
+      '',
+      '',
+      '',
+      '',
+      '',
+    ]);
+
+    surveySheetArray = surveySheetArray.concat(optionRowsArray);
+
+    // push hidden `total_marks` row to $optionRows
+    surveySheetArray.push([
+      'calculate',
+      'total_marks',
+      '',
+      '',
+      '',
+      '',
+      '',
+      '',
+      '',
+      '',
+      totalMarksCellArray.join(' + '),
+      '',
+    ]);
+
+    // push the remaining rows
+    surveySheetArray = surveySheetArray.concat([
+      [
+        'hidden',
+        'ques_number',
+        questions.result.questions.length,
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+      ],
+      [
+        'hidden',
+        'total_ques',
+        'कुल प्रश्न = ' + questions.result.questions.length,
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+      ],
+      [
+        'hidden',
+        'total_correct',
+        'कुल प्रश्न जिसका सही उत्तर दिया ${total_marks}',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+        '',
+      ],
+      ['start', 'start', '', '', '', '', '', '', '', '', '', ''],
+      ['end', 'end', '', '', '', '', '', '', '', '', '', ''],
+      ['today', 'today', '', '', '', '', '', '', '', '', '', ''],
+    ]);
+
+    settingsSheetArray.push([name, 'g2m_w1', 'no']);
+
+    return [surveySheetArray, choicesSheetArray, [], settingsSheetArray];
+  }
+
+  /**
+   * Returns the absolute path of the .xlsx file created
+   * @param questions
+   * @param filters
+   * @param filepath
+   * @return string file (absolute path of the generated XSLX form)
+   */
+  public createForm(
+    questions,
+    filters: GenerateFormDto,
+    filepath: string,
+  ): string {
+    const data = this.parseQuestions(questions, filters);
+    const surveySheetArray = data[0];
+    const choicesSheetArray = data[1];
+    const mediaSheetArray = data[2];
+    const settingsSheetArray = data[3];
+
+    const workbook = XLSX.utils.book_new();
+    const surveySheet = XLSX.utils.aoa_to_sheet(surveySheetArray);
+    const choicesSheet = XLSX.utils.aoa_to_sheet(choicesSheetArray);
+    const mediaSheet = XLSX.utils.aoa_to_sheet(mediaSheetArray);
+    const settingsSheet = XLSX.utils.aoa_to_sheet(settingsSheetArray);
+    XLSX.utils.book_append_sheet(workbook, surveySheet, 'survey');
+    XLSX.utils.book_append_sheet(workbook, choicesSheet, 'choices');
+    XLSX.utils.book_append_sheet(workbook, mediaSheet, 'media');
+    XLSX.utils.book_append_sheet(workbook, settingsSheet, 'settings');
+    XLSX.writeFileXLSX(workbook, filepath, { bookType: 'xlsx', type: 'file' });
+
+    return filepath;
+  }
+}
