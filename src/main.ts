73701d27e0b86aea79a937ee330d6386d738542f
Abhishek Mishra <abhishek@luezoid.com>
2023-01-12 13:04:10 +0530

    App's default port set to 3000 (same as exposed one in docker file)

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.use(json({ limit: '50mb' })); // accept body with up to 50mb
-  await app.listen(process.env.PORT || 3000);
+  await app.listen(3000);
 }
 bootstrap();
